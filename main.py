8c0c47e99dd08f51fc33126888c901f1ebc0c933
Syed Shahbaz Ali Shah <62151454+syedshahbazali-daata@users.noreply.github.com>
2023-07-29 15:14:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,79 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request
+import json
+from datetime import datetime
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def get_data_from_json():
+    with open(f"users-data.json", "r", encoding='utf-8') as f:
+        config = json.load(f)
+    return config
+
+
+# REST API
+@app.route("/api/users")
+def get_users():
+    return get_data_from_json()
+
+
+# ask for parameters (EMAIL, COMPANY) to create a new user
+@app.route("/api/create_user/<email>/<company>")
+def create_user(email, company):
+    if "@" not in email:
+        return {"status": "error"}
+
+    already_emails_list = get_data_from_json()
+    already_emails_list = [i['email'] for i in already_emails_list]
+    if email in already_emails_list:
+        return {"message": "email already registered"}
+
+    user_email = email
+    company_name = company
+    current_date = datetime.utcnow().strftime("%d-%m-%Y %H:%M:%S")
+    joining_date = ""
+
+    record = {
+        "email": user_email,
+        "company": company_name,
+        "current_date": current_date,
+        "joining_date": joining_date,
+    }
+
+    # add into users data
+    with open(f"users-data.json", "r", encoding='utf-8') as f:
+        config = json.load(f)
+
+    config.append(record)
+
+    with open(f"users-data.json", "w", encoding='utf-8') as f:
+        json.dump(config, f, indent=4)
+
+    return {"status": "success"}
+
+
+@app.route("/api/update_user/<email>")
+def update_user(email):
+    if "@" not in email:
+        return {"message": "wrong email"}
+
+    users_data = get_data_from_json()
+    current_date = datetime.utcnow().strftime("%d-%m-%Y %H:%M:%S")
+    for single_user_data in users_data:
+        if single_user_data['email'] == email:
+
+            if single_user_data['joining_date'] != "":
+                return {"message": "already join"}
+
+            single_user_data['joining_date'] = current_date
+            with open(f"users-data.json", "w", encoding='utf-8') as f:
+                json.dump(users_data, f, indent=4)
+
+            return {"message": "success"}
+
+    return {"message": "email not found"}
 
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
+
